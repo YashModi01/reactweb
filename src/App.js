664b7f6a041d8f9683d0eecbc6c810090a4b704e
YashModi01 <modiyash996@gmail.com>
2020-10-27 04:50:37 -0500

    created react components

--- src/App.js
+++ src/App.js
@@ -1,9 +1,20 @@
 import React from 'react'
+import { Redirect, Route, Switch } from 'react-router-dom';
+import Home from './Home'
+import Services from './Services'
+import About from './About'
+import Contact from './Contact'
 
-function App() {
+const App = () => {
   return (
     <>
-      <h1>Welcome</h1> 
+      <Switch>
+        <Route path = "/" component = {Home} exact />
+        <Route path = "/service" component = {Services} exact/>
+        <Route path = "/about" component = {About} exact />
+        <Route path = "/contact" component = {Contact} exact />
+        <Redirect to = "/" />
+      </Switch>
     </>
   );
 }
