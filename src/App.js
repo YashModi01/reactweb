87ff9d6f277a9d855cecf85655d53ccfd55d7aa7
YashModi01 <modiyash996@gmail.com>
2020-10-27 04:56:18 -0500

    import bootstrap5 in app file

--- src/App.js
+++ src/App.js
@@ -4,6 +4,7 @@ import Home from './Home'
 import Services from './Services'
 import About from './About'
 import Contact from './Contact'
+import '../node_modules/bootstrap/dist/css/bootstrap.min.css'
 
 const App = () => {
   return (
